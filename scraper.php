a3d47a32fd61e8249d31dc19742cf4682d5d9b66
vikash12345 <vikash@backgroundcheck.email>
2016-08-03 09:50:51 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,51 +1,4 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?
 	// This is a template for a PHP scraper on morph.io (https://morph.io)
 	// including some code snippets below that you should find helpful
 	require 'scraperwiki.php';
@@ -63,7 +16,8 @@
 	   
 	   // walk through the dom and extract doctor information
 	   $info['doc_name'] = $dom->find('span[id=Name]')->plaintext;
-           $info['doc_fname'] = $dom->find('span[id="FatherName"]')->plaintext;
+      print_r($info);
+	  /* $info['doc_fname'] = $dom->find('span[id="FatherName"]')->plaintext;
 	   $info['doc_dob'] = $dom->find('span[id="DOB"]')->plaintext;
 	   $info['doc_infoyear'] = $dom->find('span[id="lbl_Info"]')->plaintext;
 	   $info['doc_regnum'] = $dom->find('span[id="Regis_no"]')->plaintext;
@@ -88,7 +42,7 @@
 	    ));
 	   
 	  //clean out the dom
-	  $dom->__destruct();
+	  $dom->__destruct();*/
 	}
 	?>
 
